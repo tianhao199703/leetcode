fedeb019da3e6aa0ddfd9c8b536c2614fe73b262
do-not-ask <aatthh1971@gmail.com>
2019-10-03 21:19:40 +0800

    Update Longest Palindromic Substring.cc

--- Longest Palindromic Substring.cc
+++ Longest Palindromic Substring.cc
@@ -4,11 +4,11 @@
         int min_start = 0, max_len = 1;
         for (int i = 0; i < s.size();) {
           if (s.size() - i <= max_len / 2) break;
-          int j = i, k = i;
-          while (k < s.size()-1 && s[k+1] == s[k]) ++k; // Skip duplicate characters.
-          i = k+1;
-          while (k < s.size()-1 && j > 0 && s[k + 1] == s[j - 1]) { ++k; --j; } // Expand.
-          int new_len = k - j + 1;
+          int left = i, right = i;
+          while (right < s.size()-1 && s[right+1] == s[right]) ++right; // 一定可以构成回文的重复元素序列
+          i = right+1;//下一次起始位置
+          while (right < s.size()-1 && left > 0 && s[right + 1] == s[left - 1]) { ++right; --left; } // 向两边扩展
+          int new_len = right - left + 1;
           if (new_len > max_len) { min_start = j; max_len = new_len; }
         }
         return s.substr(min_start, max_len);
