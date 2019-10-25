d738ac60bba3cd8daea9429b1b04812ef7e20368
do-not-ask <aatthh1971@gmail.com>
2019-10-25 20:00:55 +0800

    Create Search in Rotated Sorted Array.cc

---
+++ Medium/Search in Rotated Sorted Array.cc
@@ -0,0 +1,18 @@
+int search(vector<int>& nums, int target) {
+        int lo=0,hi=nums.size()-1;
+        while(lo<hi){
+            int mid=(lo+hi)/2;
+            if(nums[mid]>nums[hi]) lo=mid+1;
+            else hi=mid;
+        }
+        int rot=lo;
+        lo=0;hi=nums.size()-1;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            int realmid=(mid+rot)%nums.size();
+            if(nums[realmid]==target)return realmid;
+            if(nums[realmid]<target)lo=mid+1;
+            else hi=mid-1;
+        }
+        return -1;
+    }
