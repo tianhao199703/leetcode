a94a8a982c8197798f31f4844178e44e90dd0159
do-not-ask <aatthh1971@gmail.com>
2019-11-12 12:42:58 +0800

    Update Subarray Sum Equals K.cc

--- Medium/Subarray Sum Equals K.cc
+++ Medium/Subarray Sum Equals K.cc
@@ -1,12 +1,13 @@
 int subarraySum(vector<int>& nums, int k) {
-        int cum=0; // cumulated sum
-        map<int,int> rec; // prefix sum recorder
-        int cnt = 0; // number of found subarray
-        rec[0]++; // to take into account those subarrays that begin with index 0
-        for(int i=0;i<nums.size();i++){
-            cum += nums[i];
-            cnt += rec[cum-k];
-            rec[cum]++;
+        unordered_map<int,int> record{{0,1}};
+        int cursum=0,count=0,n=nums.size();
+        for(int i=0;i<n;i++){
+            cursum += nums[i];
+            if(record.find(cursum-k)!=record.end()){
+                count += record[cursum-k];
+            }
+            record[cursum]++;
         }
-        return cnt;
+        return count;
     }
+
