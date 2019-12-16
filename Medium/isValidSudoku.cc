cd92635d39f730e7bfc67fa649aca4c36411a3f6
do-not-ask <aatthh1971@gmail.com>
2019-12-16 13:02:11 +0800

    Update isValidSudoku.cc

--- Medium/isValidSudoku.cc
+++ Medium/isValidSudoku.cc
@@ -1,16 +1,14 @@
-bool isValidSudoku(vector<vector<char> > &board)
-    {
-        int used1[9][9] = {0}, used2[9][9] = {0}, used3[9][9] = {0};
-        
-        for(int i = 0; i < board.size(); ++ i)
-            for(int j = 0; j < board[i].size(); ++ j)
-                if(board[i][j] != '.')
-                {
+bool isValidSudoku(vector<vector<char>>& board) {
+        vector<vector<bool>> row(9,vector<bool>(9,true)), col(9,vector<bool>(9,true)), box(9,vector<bool>(9,true));
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j] != '.'){
                     int num = board[i][j] - '0' - 1, k = i / 3 * 3 + j / 3;
-                    if(used1[i][num] || used2[j][num] || used3[k][num])
+                    if(!(col[i][num] && row[j][num] && box[k][num]))
                         return false;
-                    used1[i][num] = used2[j][num] = used3[k][num] = 1;
+                    col[i][num] = row[j][num] = box[k][num] = false;
                 }
-        
+            }
+        }
         return true;
     }
