d74bfdab1e6db49bee7f5e2d6395c8b15794b417
do-not-ask <aatthh1971@gmail.com>
2020-02-24 23:10:30 +0800

    Update Word Ladder II.cc

--- Medium/Word Ladder II.cc
+++ Medium/Word Ladder II.cc
@@ -6,12 +6,13 @@ vector<vector<string>> findLadders(string beginWord, string endWord, vector<stri
 			return paths;
 		}
         if(find(wordList.begin(),wordList.end(),endWord)==wordList.end() && beginWord!=endWord){return paths;}
+	//以上是处理边界情况
         unordered_set<string> dict;
         for(auto i:wordList){dict.insert(i);}
         unordered_set<string> words1, words2;
 		words1.insert(beginWord);
 		words2.insert(endWord);
-		unordered_map<string, vector<string>> nexts;
+		unordered_map<string, vector<string>> nexts;//每一个单词和他距离为1的其他单词的映射图
 		bool words1IsBegin = false;
         if (findLaddersHelper(words1, words2, dict, nexts, words1IsBegin))
 			getPath(beginWord, endWord, nexts, path, paths);
@@ -28,12 +29,12 @@ vector<vector<string>> findLadders(string beginWord, string endWord, vector<stri
 		for (auto it = words2.begin(); it != words2.end(); ++it)
 			dict.erase(*it);
         unordered_set<string> words3;
-		bool reach = false;
+		bool reach = false;//判断是否能达到endword的变量
         for (auto it = words1.begin(); it != words1.end(); ++it) {
 			string word = *it;
 			for (auto ch = word.begin(); ch != word.end(); ++ch) {
 				char tmp = *ch;
-                for (*ch = 'a'; *ch <= 'z'; ++(*ch))
+                for (*ch = 'a'; *ch <= 'z'; ++(*ch))//当前单词所有距离为1的单词变种
 					if (*ch != tmp)
 						if (words2.find(word) != words2.end()) {
 							reach = true;
@@ -48,6 +49,7 @@ vector<vector<string>> findLadders(string beginWord, string endWord, vector<stri
         }
         return reach || findLaddersHelper(words2, words3, dict, nexts, words1IsBegin);
     }
+//DFS打印
 	void getPath(string beginWord, string &endWord, unordered_map<string, vector<string> > &nexts, vector<string> &path, vector<vector<string>> &paths) {
 		if(beginWord == endWord)
 			paths.push_back(path);
