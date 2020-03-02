8489fa8acdd9d21d25c3f6568dc3cbf49313d7ad
do-not-ask <aatthh1971@gmail.com>
2020-03-02 23:29:46 +0800

    Update wordbreakII.cc

--- Hard/wordbreakII.cc
+++ Hard/wordbreakII.cc
@@ -1,14 +1,22 @@
-vector<string> wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> wordD;
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
         for(auto &i:wordDict){wordD.insert(i);}
-        unordered_map<int, vector<string>> memo {{s.size(), {""}}};
-        function<vector<string>(int)> sentences = [&](int i) {
-            if (!memo.count(i))
-                for (int j=i+1; j<=s.size(); j++)
-                    if (wordD.count(s.substr(i, j-i)))
-                        for (string tail : sentences(j))
-                            memo[i].push_back(s.substr(i, j-i) + (tail=="" ? "" : ' ' + tail));
-            return memo[i];
-        };
-        return sentences(0);
+        memo[s.size()] = {""};
+        return dp(s,0);
     }
+    vector<string> dp(string s, int i){
+        if (memo.find(i) == memo.end()){
+            for(int j=i+1; j<=s.size();j++){
+                if (wordD.find(s.substr(i, j-i)) != wordD.end()){
+                    auto res = dp(s,j);
+                    for(auto tail: res){
+                        memo[i].push_back(s.substr(i, j-i) + (tail==""?"":' '+tail));//对应起点的字符串存储下来
+                    }
+                }
+            }
+        }
+        return memo[i];
+    }
+private:
+    unordered_map<int, vector<string>> memo;
+    unordered_set<string> wordD;
