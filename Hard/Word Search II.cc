136b6e51fd9a0dbc369aea25bf6cffdfa42de545
do-not-ask <aatthh1971@gmail.com>
2020-03-06 19:54:18 +0800

    Update Word Search II.cc

--- Hard/Word Search II.cc
+++ Hard/Word Search II.cc
@@ -1,32 +1,33 @@
+//Trie树代码见https://github.com/tianhao199703/algorithm/blob/master/Tree/Trie.cc
 class Solution {
 public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-        Trie* trie = new Trie(words);
-        TrieNode* root=trie->getRoot();
-        set<string> result_set;
-        for(int x=0; x<board.size(); ++x)
-            for(int y=0; y<board[0].size(); ++y)
-                findWords(board, x, y, root, "", result_set);
-        
+        Trie trie(words);
+        TrieNode* root=trie.getRoot();
+        int m = board.size(),n = board[0].size();
+        for(int i=0; i<m; i++)
+            for(int j=0; j<n; j++)
+                findWords(board, i, j, root, "");
         vector<string> result;
-        for(auto it:result_set)    result.push_back(it);
-        return result;        
+        for(auto it:result_set) result.push_back(it);
+        return result; 
     }
-private:
-    void findWords(vector<vector<char>>& board, int x, int y, TrieNode* root, string word, set<string>& result){
-        if(x<0||x>=board.size()||y<0||y>=board[0].size() || board[x][y]==' ') return;
+    void findWords(vector<vector<char>>& board, int i, int j, TrieNode* root, string word){
+        if(i<0||i>=board.size()||j<0||j>=board[0].size() || board[i][j]==' ') return;
         
-        if(root->children[board[x][y]-'a'] != NULL){
-            word=word+board[x][y];
-            root=root->children[board[x][y]-'a']; 
-            if(root->is_end) result.insert(word);
-            char c=board[x][y];
-            board[x][y]=' ';
-            findWords(board, x+1, y, root, word, result);
-            findWords(board, x-1, y, root, word, result);
-            findWords(board, x, y+1, root, word, result);
-            findWords(board, x, y-1, root, word, result);
-            board[x][y]=c;        
+        if(root->children[board[i][j]-'a'] != nullptr){
+            word += board[i][j];
+            root = root->children[board[i][j]-'a']; 
+            if(root->is_end) result_set.insert(word);
+            char c = board[i][j];
+            board[i][j]=' ';//backtrack
+            findWords(board, i+1, j, root, word);
+            findWords(board, i-1, j, root, word);
+            findWords(board, i, j+1, root, word);
+            findWords(board, i, j-1, root, word);
+            board[i][j] = c;        
         }
     }
+private:
+    set<string> result_set;
 };
