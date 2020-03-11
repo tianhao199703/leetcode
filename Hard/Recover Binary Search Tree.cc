3378e9ae41721ad8e6bac6a643665e6a3729fc82
do-not-ask <aatthh1971@gmail.com>
2020-03-11 16:05:28 +0800

    Update Recover Binary Search Tree.cc

--- Hard/Recover Binary Search Tree.cc
+++ Hard/Recover Binary Search Tree.cc
@@ -2,34 +2,33 @@
         TreeNode *pre = nullptr;
         TreeNode *first = nullptr, *second = nullptr;
         TreeNode *temp = nullptr;
-		while(root != nullptr){
-			if(root->left != nullptr){
-				temp = root->left;
-				while(temp->right!=nullptr && temp->right != root)
-					temp = temp->right;
-				if(temp->right!=nullptr){
-				    if(pre!=nullptr && pre->val > root->val){
-				        if(first==nullptr){first = pre;second = root;}
-				        else{second = root;}
-				    }
-				    pre = root;
-					temp->right = nullptr;
-					root = root->right;
-				}
-                else{
-					temp->right = root;
-					root = root->left;
-				}
-			}
-            else{
+	while(root != nullptr){
+		if(root->left != nullptr){
+			temp = root->left;
+			while(temp->right!=nullptr && temp->right != root) temp = temp->right;
+			if(temp->right!=nullptr){
+				//11-15行的代码是用来判断当前节点是否比之前保存的节点值要小，说明被交换了，因为中序遍历是升序
 				if(pre!=nullptr && pre->val > root->val){
-				    if(first==nullptr){first = pre;second = root;}
-				    else{second = root;}
+					if(first==nullptr){first = pre;second = root;}
+					else{second = root;}
 				}
 				pre = root;
+				temp->right = nullptr;
 				root = root->right;
 			}
+                	else{
+				temp->right = root;
+				root = root->left;
+			}
 		}
-		// swap two node values;
-		swap(first->val,second->val);
+            	else{
+			if(pre!=nullptr && pre->val > root->val){
+				if(first==nullptr){first = pre;second = root;}
+				else{second = root;}
+			}
+			pre = root;
+			root = root->right;
+		}
+	}
+	swap(first->val,second->val);
     }
