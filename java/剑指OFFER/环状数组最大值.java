e5622620e40873caf278db57fbd79c6397fa3039
do-not-ask <aatthh1971@gmail.com>
2023-05-09 15:37:03 +0800

    Create 环状数组最大值.java

---
+++ java/剑指OFFER/环状数组最大值.java
@@ -0,0 +1,12 @@
+//按照最大值和总和-最小值，两种情况的最大值
+public int maxSubarraySumCircular(int[] nums) {
+        int total = 0, maxSum = nums[0], curMax = 0, minSum = nums[0], curMin = 0;
+        for (int a : nums) {
+            curMax = Math.max(curMax + a, a);
+            maxSum = Math.max(maxSum, curMax);
+            curMin = Math.min(curMin + a, a);
+            minSum = Math.min(minSum, curMin);
+            total += a;
+        }
+        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
+    }
