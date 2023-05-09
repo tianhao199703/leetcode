d167f95b4f693c96c183adc9edc796784d67121b
do-not-ask <aatthh1971@gmail.com>
2023-05-09 12:07:55 +0800

    Update singleNumber.cc

--- Medium/singleNumber.cc
+++ Medium/singleNumber.cc
@@ -3,8 +3,10 @@ int singleNumber(vector<int>& nums) {
         int x1 = 0, x2 = 0, mask = 0;//
      
     for (int i : nums) {
-        x2 ^= x1 & i;//此处是固定的 xm ^= xm-1 & xm-2 ... & i;
+        // 这部分是计算每位的1的数量，有进位关系
+	x2 ^= x1 & i;//此处是固定的 xm ^= xm-1 & xm-2 ... & i;
         x1 ^= i;
+	// mask用来保证当1出现的次数达到k值时，全计数器归0    
         mask = ~(x1 & x2);//此处的mask里面是根据k的二进制来构造的。如果k=5（101）则mask = ~(x1 & ~x2 & x3);
         x2 &= mask;
         x1 &= mask;
